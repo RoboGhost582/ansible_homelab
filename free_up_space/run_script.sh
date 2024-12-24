e21c73ea0905f08622106883c59511b0682f8186
Robo <email@localhost.com>
2024-12-24 00:56:30 -0500

    .

--- free_up_space/run_script.sh
+++ free_up_space/run_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+du -h --total /data/ --max-depth=1  2>/dev/null | grep -e /data/downloads -e total
 
 rm -rf /data/downloads/qbittorrent/completed/*
 rm -rf /data/downloads/qbittorrent/incompleted/*
-rm -rf /data/downloads/qbittorrent/torrents/*  
+rm -rf /data/downloads/qbittorrent/torrents/* 
+
+du -h --total /data/ --max-depth=1  2>/dev/null | grep -e /data/downloads -e total
