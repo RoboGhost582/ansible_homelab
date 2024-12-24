d65d9e5370a7cf05e6317fca29e6e085573a9715
Robo <email@localhost.com>
2024-12-24 00:44:14 -0500

    .

--- free_up_space/run_script.sh
+++ free_up_space/run_script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-du -h /data/     
 
 rm -rf /data/downloads/qbittorrent/completed/*
 rm -rf /data/downloads/qbittorrent/incompleted/*
-rm -rf /data/downloads/qbittorrent/torrents/*
-
-du -h /data/     
+rm -rf /data/downloads/qbittorrent/torrents/*  
