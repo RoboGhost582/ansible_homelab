eb63e2e95432f68a36b45864c78eef23ff395ea0
Robo <email@localhost.com>
2024-12-24 14:56:22 -0500

    .

---
+++ gitea/run_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p /docker/pi-hole/pihole
+mkdir -p /docker/pi-hole/dnsmasq.d
+mkdir -p /docker/gitea/etc/timezone
+mkdir -p /docker/gitea/etc/localtime
+mkdir -p /docker/postgres
